fff1f417995ef7fa0cbcfaca24badf6d9d6299e0
salayhin <salayhin@gmail.com>
2013-06-17 16:15:22 +0600

    Cart controller added

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -18,6 +18,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+
+    assert_select 'h1', 'Listing products'
   end
 
   test "should get new" do
