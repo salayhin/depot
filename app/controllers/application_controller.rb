97add3cde0dd1d5f7199f32cec1f21a6ec557b10
salayhin <salayhin@gmail.com>
2013-06-19 15:53:50 +0600

    User Added complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :authorize
+
   private
   def current_cart
     Cart.find(session[:cart_id])
@@ -9,4 +11,12 @@ class ApplicationController < ActionController::Base
     session[:cart_id] = cart.id
     cart
   end
+
+  protected
+  def authorize
+    unless User.find_by_id(session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    end
+  end
+
 end
